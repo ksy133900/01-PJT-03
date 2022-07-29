9a84935649026b9688425aaeded4a776c0de6bac
ksy133900 <tkdgns13578@gmail.com>
2022-07-29 17:32:44 +0900

    파이썬 모의고사

--- 1회차/김상훈/5_신용카드만들기2.py
+++ 1회차/김상훈/5_신용카드만들기2.py
@@ -1,3 +1,20 @@
 import sys
 
 sys.stdin = open("_신용카드만들기2.txt")
+
+
+T = int(input())
+for _ in range(1,T+1):
+
+    card_numbers = input()
+    list_card =[]
+    for i in card_numbers:
+        list_card.append(i)
+        
+        if card_numbers[0] == '3' or card_numbers[0] == '4' or card_numbers[0] == '5' or card_numbers[0] == '6' or card_numbers[0] == '9' and list_card =='-':
+            m = 1
+        else :
+            m = 0
+    
+    print(f'#{_} {m}')    
+
